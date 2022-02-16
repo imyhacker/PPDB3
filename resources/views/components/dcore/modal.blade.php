37cb75bd843f80965423a37cb12e441c611aac59
imyhacker <arifarhan1602@gmail.com>
2022-02-16 15:24:23 +0700

    Update Ntah Sampai Mana

--- resources/views/components/dcore/modal.blade.php
+++ resources/views/components/dcore/modal.blade.php
@@ -97,3 +97,33 @@
         </div>
     </div>
 </div>
+
+
+
+<div class="modal fade" id="tyt" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah File Informasi</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{route('tfinfo')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Kode Video</label>
+                        <input type="text" class="form-control" name="jinfo" placeholder="Judul File Informasi">
+                        <sup class="font-success">https://www.youtube.com/watch?v=<font class="font-danger">rGeuWHX4X2E</font></sup>
+                    </div>
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Upload File</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
